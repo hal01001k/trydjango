aaeb2044d9aa64cf5213d68b84c9847c1b3f694e
hal01001k <hiteshdaksh@yahoo.co.uk>
2020-09-08 21:53:01 +0530

    urls

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(*args,**kwargs):
     return HttpResponse("<h1>Hello World!</h1>")
+
+def contact_view(*args,**kwargs):
+    return HttpResponse("<h1>Contact page</h1>")
+
+def about_view(*args,**kwargs):
+    return HttpResponse("<h1>About page</h1>")
+
+def social_view(*args,**kwargs):
+    return HttpResponse("<h1>Social page</h1>")
