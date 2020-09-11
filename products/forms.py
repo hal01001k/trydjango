4de9a07ccf9e3f8137c6f455458892e948b21e68
hal01001k <hiteshdaksh@yahoo.co.uk>
2020-09-11 19:31:41 +0530

    RawProduct View

--- products/forms.py
+++ products/forms.py
@@ -10,3 +10,19 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+class RawProductForm(forms.Form):
+    title       = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Text Here"}))
+    description = forms.CharField(
+                        required=False,
+                        widget=forms.Textarea(
+                            attrs={
+                                "placeholder":"Discription Here",
+                                "class":"new-class-name two",
+                                "rows":6,
+                                "cols":40
+                            }
+                        )
+    )
+    price       = forms.DecimalField()
+
--- products/views.py
+++ products/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 from .models import Product
 
 # Create your views here.
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
+    my_form = RawProductForm()
+    if request.method == "POST":
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+        else:
+            print(my_form.errors)
     context = {
-        'form': form
+        'form': my_form
     }
-    return render(request, "products/product_create.html",context)
+    return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
